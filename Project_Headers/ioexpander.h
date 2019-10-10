67739a8c0e2abd84271908cdb8b27b9686487ad5
Ruan Swanepoel <40282616+RewForeN@users.noreply.github.com>
2019-10-10 12:07:39 +1100

    Add files via upload

---
+++ Project_Headers/ioexpander.h
@@ -0,0 +1,14 @@
+
+#ifndef PROJECT_HEADERS_IOEXPANDER_H_
+#define PROJECT_HEADERS_IOEXPANDER_H_
+
+
+class IoExpander {
+
+public:
+	void init();
+
+};
+
+
+#endif /* PROJECT_HEADERS_IOEXPANDER_H_ */
