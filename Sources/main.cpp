55e3027a95d9b22868a5e5bf8440c2b622a5eac9
Ruan Swanepoel <40282616+RewForeN@users.noreply.github.com>
2019-10-10 12:08:27 +1100

    Update main.cpp

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,11 +1,21 @@
 
 #include "hardware.h"
+#include "i2c.h"
+#include "mma845x.h"
+#include "ioexpander.h"
 
 using namespace USBDM;
 
+IoExpander io;
 
 
 int main() {
+
+	for (;;) {
+
+
+
+	}
 
 	return 0;
 
