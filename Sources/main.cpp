0b5a7028e4bebbfe94963cc6352952e0b186fa0e
Ruan Swanepoel <40282616+RewForeN@users.noreply.github.com>
2019-10-30 21:16:21 +1100

    Update main.cpp

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,180 +1,280 @@
+
 #include "hardware.h"
-#include "mma845x.h"
-#include <math.h>
-#include "system.h"
-#include "derivative.h"
+#include "math.h"
 #include "i2c.h"
-#include "delay.h"
+#include "mma845x.h"
 #include "ioexpander.h"
-//#include "pit.h"
-//#include "smc.h"
+#include "pit.h"
+
+#define PERIODOF(x) (1/x)
 
 using namespace USBDM;
 
+// PIT timer
+using Timer = Pit;
+using TmrChGameLogic = Timer::Channel<0>;
+using TmrChScorer = Timer::Channel<1>;
+
+// Accelerometer
+I2c0 i2c0;
+MMA845x accelerometer(i2c0, MMA845x::AccelerometerMode_2Gmode);
+
+// IO Expander API class
 IoExpander io;
-I2c0       i2c0;
 
-MMA845x  accelerometer(i2c0, MMA845x::AccelerometerMode_2Gmode);
+static constexpr float MAX_TILT = 2000;
+static constexpr float MIN_TILT = -2000;
+static constexpr int MAX_OFFSET = 1;
+static constexpr int MIN_OFFSET = -1;
+static constexpr int LED_START_POS = 3;
 
-//using Timer        = Pit;
-//using TimerChannel = Timer::Channel<0>;
+static constexpr float gameScorerSpeed = 1 * Hz;
+float gameLogicSpeed = 10 * Hz;
 
-static constexpr int MAX_OFFSET = 4;
-static constexpr int MIN_OFFSET = -4;
-static const int AXIS_MAX = 2000;
-static const int AXIS_MIN = -2000;
+float acc = 0.0f;
+float vel = 0.0f;
+float pos = LED_START_POS;
 
-static int LED_Position = 0;
-static int oldLED       = 0;
-static int offset       = 0;
-static float period     = 0.1;
-static bool STARTGAME   = true;
+uint8_t score = 0;
+bool isGameRunning = false;
 
-//Function Prototypes
-void InitialiseAccelerometer();
-void InitialisePIT();
-void AxisYConversion();
-void randomWalk();
+// Function Prototypes
+void startGame();
+void showScore();
+void gameOver();
+void gameLoop();
+void initialisePIT();
+void initialiseAccelerometer();
+float getAcceleration();
+int randomWalk();
+void clamp(int&, int, int);
+void clamp(float&, float, float);
+
 
 int main() {
-   InitialiseAccelerometer();
-   //InitialisePIT();
-   io.setAllPinsAsOutput();
 
-   int score;
+	// Initial setup
+	initialiseAccelerometer();
+	io.setAllPinsAsOutput();
+	startGame();
 
-   for (;;) {
-      if (STARTGAME){
-         console.writeln("Balance Game").writeln("Starts in...");
-         waitMS(1000);
-         for (int countDown = 3;countDown > 0; --countDown){
-            console.writeln(countDown);
-            waitMS(1000);
-         }
-         console.writeln("Start!");
-         STARTGAME = false;
-         score  = 0;
-         offset = 0;
-      }
+	for (;;) {
 
+		console.write("Score = ").writeln(score);
+		console.write("Speed = ").write(gameLogicSpeed).writeln("Hz");
 
-      AxisYConversion();
-      waitMS(500);
+		waitMS(100);
 
-      // Scoring condition
-      if (LED_Position == 3 || LED_Position == 4){
-         score++;
-      }
+	}
 
-      // Increases the frequency of the callback 'offset'
-      if (score%25 == 0){
-     	 period *= 2;
-     	 //TimerChannel::setPeriod(period/2 * seconds);
-     	 randomWalk();
-      }
+	return 0;
 
-
-      console.write("Score = ").write(score).write("; Speed = ").writeln(period);
-
-
-      // Lost/End Game condition
-      if (LED_Position <= 0 || LED_Position >= 7) {
-    	 console.writeln("You have Lost").write("You Scored ").writeln(score);
-
-    	 //MAKE THE LEDS GLOW
-         //;
-
-    	 console.writeln("Please reset position.");
-    	 while (LED_Position != 3 && LED_Position != 4){
-    		 AxisYConversion();
-    	 }
-
-    	 console.writeln("Beginning to Start...");
-    	 waitMS(4000);
-    	 STARTGAME = true;
-      }
-   }
-
-   return 0;
 }
 
 /**
- * Initialise the Accelerometer
+ * Sets up a new game and shows a startup animation
  */
-void InitialiseAccelerometer(){
-   console.writeln("Starting\n");
-   console.write("Device ID = 0x").write(accelerometer.readID(), Radix_16).writeln("(should be 0x1A)");
+void startGame() {
 
-   // Check if any USBDM error yet (constructors)
-   checkError();
+	io.allPinsHigh();
+	initialisePIT();
 
-   console.write("Doing simple calibration\n"
-		 "Make sure the device is level!\n");
-   waitMS(2000);
+	acc = 0;
+	vel = 0;
+	pos = LED_START_POS;
+	score = 0;
+	gameLogicSpeed = 10*Hz;
+	TmrChGameLogic::configure(PERIODOF(gameLogicSpeed), PitChannelIrq_Enabled);
 
-   // Error Case
-   if (accelerometer.calibrateAccelerometer() != E_NO_ERROR) {
-      console.write("Calibration failed!\n");
-      __asm__("bkpt");
-   }
+	waitMS(500);
+	isGameRunning = true;
 
-   // Make sure we have new values
-   waitMS(100);
+}
 
-   console.write("After calibration\n");
+/**
+ * Shows the score in binary on the LED bar
+ */
+void showScore() {
+
+	io.customPinValues(score);
+
+}
+
+/**
+ * Ends the game and restarts it
+ */
+void gameOver() {
+
+	isGameRunning = false;
+	io.allPinsHigh();
+	waitMS(1000);
+	showScore();
+	waitMS(3000);
+	startGame();
+
+}
+
+/**
+ * This is the main game logic. This is where the LED position gets updated and displayed.
+ * This is a callback from a PIT channel
+ */
+void gameLoop() {
+
+	if (!isGameRunning) return;
+
+	// Game logic
+	acc = getAcceleration(); 	// Get acceleration from accelerometer
+	vel += acc;					// Add acceleration to velocity of LED
+	vel *= 0.9f;				// Give the velocity some drag
+	clamp(vel, -0.5f, 0.5f);	// Limit min and max velocity
+	pos += vel;					// Add velocity to position of LED
+	pos += randomWalk() / 2.0;	// Apply the random walk to the position
+
+	int position = (int)round(pos);
+
+	// Display LED position
+	io.allPinsLow();
+	io.pinHigh(position);
+
+	// Check if LED is out of bounds
+	if (pos > 7 || pos < 0) gameOver();
+
+}
+
+/**
+ * Callback method that runs every second
+ */
+void gameScorer() {
+
+	if (!isGameRunning) return;
+
+	// Increase score
+	score++;
+
+	// Increase game speed
+	gameLogicSpeed += 1 * Hz;
+	TmrChGameLogic::configure(PERIODOF(gameLogicSpeed), PitChannelIrq_Enabled);
+
 }
 
 /**
  * Initialise the PIT
  */
-/*
-void InitialisePIT(){
-   Timer::configure(PitDebugMode_Stop);
-   // Set handler programmatically
-   TimerChannel::setCallback(randomWalk);
-   // Configures the the frequency of the calling of  'randomWalk()'
-   TimerChannel::configure(period * seconds, PitChannelIrq_Enabled);
-   TimerChannel::enableNvicInterrupts(NvicPriority_Normal);
-   // Check for errors so far
-   checkError();
+void initialisePIT() {
+
+	Timer::configure(PitDebugMode_Stop);
+
+	TmrChGameLogic::setCallback(gameLoop);
+	TmrChGameLogic::enableNvicInterrupts(NvicPriority_Normal);
+	TmrChGameLogic::configure(PERIODOF(gameLogicSpeed), PitChannelIrq_Enabled);
+
+	TmrChScorer::setCallback(gameScorer);
+	TmrChScorer::enableNvicInterrupts(NvicPriority_Normal);
+	TmrChScorer::configure(PERIODOF(gameScorerSpeed), PitChannelIrq_Enabled);
+
+	// Check for errors so far
+	checkError();
+
 }
-*/
+
 /**
- * Converts the readings from the accelerometer to be usable for the LEDs
+ * Initialise the Accelerometer
  */
-void AxisYConversion(){
-   //Gets the currents values of the Accelerometer
-   int accelStatus;
-   int16_t accelX,accelY,accelZ;
-   accelerometer.readAccelerometerXYZ(accelStatus, accelX, accelY, accelZ);
+void initialiseAccelerometer(){
 
-   //Modifies 'accelY' for usage for the LED
-   LED_Position = int(3.5 - accelY * (MAX_OFFSET - MIN_OFFSET)/(AXIS_MAX - AXIS_MIN));
+	console.write("Device ID = 0x").write(accelerometer.readID(), Radix_16).writeln(" (should be 0x1A)");
 
-   //Testing
-   console.write("AccelY = ").write(accelY).write("; LED = ").writeln(LED_Position);
+	checkError(); // Check if any USBDM error yet (constructors)
+	console.write("Doing simple calibration\nMake sure the device is level!\n");
+	waitMS(2000);
 
-   //Applies randomWalk value
-   //LED_Position += offset;
+	ErrorCode err = accelerometer.calibrateAccelerometer(); // Calibrate
 
-   //Display where the current LED is
-   io.pinLow(oldLED);
-   io.pinHigh(LED_Position);
-   oldLED = LED_Position;
+	// Check calibration error
+	if (err != E_NO_ERROR) {
+	  console.writeln("Calibration failed!");
+	  __asm__("bkpt");
+	}
+
+	// Make sure we have new values
+	waitMS(100);
+	console.writeln("Calibration complete.");
+
+}
+
+/**
+ * Get the acceleration of the LED based on the accelerometer's Y-axis value
+ *
+ * @return - Acceleration value
+ */
+float getAcceleration() {
+
+	// Get the currents values of the Accelerometer
+	int status;
+	int16_t accX, accY, accZ;
+	accelerometer.readAccelerometerXYZ(status, accX, accY, accZ);
+
+	int value = accY;
+	clamp(value, MIN_TILT, MAX_TILT);
+
+	return -value / 2000.0f;
+
 }
 
 /**
  * Provides an offset value that may vary by up to +/- 1 on each call.
+ *
+ * @return - offset value in range [MIN_OFFSET .. MAX_OFFSET]
  */
-void randomWalk() {
-   switch(rand()%2) {
-      case 0: offset -= 1; break;
-      case 1: offset += 1; break;
-   }
-   if (offset < MIN_OFFSET) {
-      offset = MIN_OFFSET;
-   }
-   if (offset > MAX_OFFSET) {
-      offset = MAX_OFFSET;
-   }
+int randomWalk() {
+
+	static int offset = 0;
+
+	switch(rand() % 2) {
+		case 0:
+			offset -= 1;
+			break;
+		case 1:
+			offset += 1;
+			break;
+	}
+
+	if (offset < MIN_OFFSET) {
+		offset = MIN_OFFSET;
+	}
+
+	if (offset > MAX_OFFSET) {
+		offset = MAX_OFFSET;
+	}
+
+	return offset;
+
+}
+
+/**
+ * Limit/clamp a given value within a certain range
+ *
+ * @param[out] n - Value to be limited
+ * @param[in] lower - Lower value of the limited range
+ * @param[in] upper - Upper value of the limited range
+ */
+void clamp(int &n, int lower, int upper) {
+
+	if (n >= upper) n = upper;
+	else if (n <= lower) n = lower;
+
+}
+
+/**
+ * Limit/clamp a given value within a certain range
+ *
+ * @param[out] n - Value to be limited
+ * @param[in] lower - Lower value of the limited range
+ * @param[in] upper - Upper value of the limited range
+ */
+void clamp(float &n, float lower, float upper) {
+
+	if (n >= upper) n = upper;
+	else if (n <= lower) n = lower;
+
 }
